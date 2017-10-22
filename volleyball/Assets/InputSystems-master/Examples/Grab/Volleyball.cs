f40e39993596d2ee68463eeaee28d0acc2f1020e
Marcus Guimaraes <mzg218@nyu.edu>
2017-10-22 19:40:01 -0400

    rudimentary ball physics

--- volleyball/Assets/InputSystems-master/Examples/Grab/Volleyball.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[RequireComponent (typeof(Collider), typeof(Rigidbody))]
-public class Volleyball : MonoBehaviour {
-  //tag class
-}
