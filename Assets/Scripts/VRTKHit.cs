eceb4d96b1205896d0e9368a697355117aa4052f
Marcus Guimaraes <mzg218@nyu.edu>
2017-11-12 15:29:59 -0500

    added VRTK

---
+++ Assets/Scripts/VRTKHit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using VRTK;
+
+public class VRTKHit : MonoBehaviour {
+
+  public float impactScalar = 120f;
+  public float upForce = 100f;
+
+  private VRTK_ControllerReference controller;
+
+	// Use this for initialization
+	void Start () {
+    VRTK_ControllerReference.GetControllerReference(gameObject);
+	}
+	
+	void OnCollisionEnter(Collision col) {
+    Volleyball ball = col.gameObject.GetComponent<Volleyball>();
+    if (ball != null) {
+      float handForce = VRTK_DeviceFinder.GetControllerVelocity(controller).magnitude * impactScalar;
+      ContactPoint point = col.contacts[0];
+      col.rigidbody.AddForceAtPosition( (-point.normal.normalized * handForce) , point.point);
+      col.rigidbody.AddForce(upForce * Vector3.up);
+    }
+  }
+}
